9e6200972741955dc6be6d7e2d0210021ffea72d
azami-ho <hossein.azami@gmail.com>
2020-06-27 15:14:44 +0300

    1

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 using WebApp.Models;
 
-namespace WebApp.Controllers
+namespace TurkCRM.WebApp.Controllers
 {
     public class HomeController : Controller
     {
